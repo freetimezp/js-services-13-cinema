981a1d872a89e6d4daf57be72b50df5cbc29c821
Evgen Onoprienko <51513653+freetimezp@users.noreply.github.com>
2024-03-30 12:42:22 +0200

    Update Banner.jsx
    api data

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -11,7 +11,7 @@ function Banner() {
     const [movies, setMovies] = useState([]);
 
     const fetchData = () => {
-        fetch('http://localhost:3000/data/movieData.json')
+        fetch('https://js-services-13-cinema.vercel.app/data/movieData.json')
             .then(res => res.json())
             .then(data => setMovies(data))
             .catch(e => console.log(e.message));
