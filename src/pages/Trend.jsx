dddad7b313865c90ee535630470c9dfd79778400
Evgen Onoprienko <51513653+freetimezp@users.noreply.github.com>
2024-03-30 12:46:48 +0200

    Update Trend.jsx
    api data

--- src/pages/Trend.jsx
+++ src/pages/Trend.jsx
@@ -13,7 +13,7 @@ function Trend() {
     const [slides, setSlides] = useState([]);
 
     const fetchData = () => {
-        fetch('http://localhost:3000/data/movieData.json')
+        fetch('https://js-services-13-cinema.vercel.app/data/movieData.json')
             .then(res => res.json())
             .then(data => setSlides(data))
             .catch(e => console.log(e.message));
